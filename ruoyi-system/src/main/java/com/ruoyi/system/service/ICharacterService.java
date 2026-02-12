61779be24a215d4d4f8ca993ae5a9c55d4de5477
IfeyChan702 <152843056+IfeyChan702@users.noreply.github.com>
2026-02-12 15:46:58 +0800

    Update ICharacterService interface definition

--- ruoyi-system/src/main/java/com/ruoyi/system/service/ICharacterService.java
+++ ruoyi-system/src/main/java/com/ruoyi/system/service/ICharacterService.java
@@ -3,13 +3,13 @@ package com.ruoyi.system.service;
 import com.ruoyi.system.domain.StoryCharacter;
 import java.util.List;
 
-public interface IStoryCharacterService {
-    public StoryCharacter selectStoryCharacterById(Long id);
-    public List<StoryCharacter> selectStoryCharacterList(StoryCharacter storyCharacter);
-    public List<StoryCharacter> selectStoryCharacterListByUserId(Long userId);
-    public int insertStoryCharacter(StoryCharacter storyCharacter);
-    public int updateStoryCharacter(StoryCharacter storyCharacter);
-    public int deleteStoryCharacterById(Long id);
-    public int deleteStoryCharacterByIds(Long[] ids);
-    public List<StoryCharacter> searchStoryCharacters(Long userId, String keyword);
+public interface ICharacterService {
+    public StoryCharacter selectCharacterById(Long id);
+    public List<StoryCharacter> selectCharacterList(StoryCharacter storyCharacter);
+    public List<StoryCharacter> selectCharacterListByUserId(Long userId);
+    public int insertCharacter(StoryCharacter storyCharacter);
+    public int updateCharacter(StoryCharacter storyCharacter);
+    public int deleteCharacterById(Long id);
+    public int deleteCharacterByIds(Long[] ids);
+    public List<StoryCharacter> searchCharacters(Long userId, String keyword);
 }
